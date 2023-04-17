878ddeabee45aedeae68564c4c2a4c02cfd0dcd2
El Diego <SirDaXll@users.noreply.github.com>
2023-04-17 13:53:11 -0400

    Fruta

---
+++ ejercicio6.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QLineEdit, QVBoxLayout, QWidget
+
+class Primo(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Lista de frutas')
+        caja = QVBoxLayout()
+
+        self.texto = QLabel('Ingresa una fruta a la lista:')
+        self.fruta = QLineEdit(self)
+
+        self.boton = QPushButton('Agregar')
+
+        self.boton.clicked.connect(self.agregar)
+
+        self.lista = QLabel("Lista de frutas:\n")
+
+        caja.addWidget(self.texto)
+        caja.addWidget(self.fruta)
+        caja.addWidget(self.boton)
+        caja.addWidget(self.lista)
+
+        ventana = QWidget()
+        ventana.setLayout(caja)
+        self.setCentralWidget(ventana)
+
+    def agregar(self):
+        item = self.fruta.text()
+
+        self.lista.setText(self.lista.text() + item + "\n")
+
+        self.fruta.clear()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Primo()
+    window.show()
+    sys.exit(app.exec())
